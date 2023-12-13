6a15b691c85eadb823a0e308cf61d6cddd9e7103
Scott Parrish <slparrish@gmail.com>
2023-12-12 22:08:01 -0500

    added registration and included sample db

--- raid220/urls.py
+++ raid220/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("eventapp/", include("eventapp.urls")),
+    path("users/", include("users.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
